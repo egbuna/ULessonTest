fcef884ac974d6bc16eb55540c85329524b6b046
egbuna <egbunachukwudi49@gmail.com>
2021-01-15 23:37:12 +0100

    feat: fetch and display data

--- app/src/main/java/com/example/ulessontest/ui/dashboard/DashBoardFragment.kt
+++ app/src/main/java/com/example/ulessontest/ui/dashboard/DashBoardFragment.kt
@@ -2,8 +2,11 @@ package com.example.ulessontest.ui.dashboard
 
 import android.util.Log
 import android.view.View
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.core.di.network.NetworkStatus
 import com.example.ulessontest.R
 import com.example.ulessontest.databinding.FragmentDashboardBinding
@@ -14,11 +17,19 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class DashBoardFragment : BaseFragment(R.layout.fragment_dashboard) {
 
-    val viewModel: DashBoardViewModel by viewModels()
-    val binding by viewBinding(FragmentDashboardBinding::bind)
+    private val viewModel: DashBoardViewModel by viewModels()
+    private val binding by viewBinding(FragmentDashboardBinding::bind)
+
+    private lateinit var subjectAdapter: SubjectAdapter
 
     override fun setUp(view: View) {
         setUpObserver()
+        subjectAdapter = SubjectAdapter {
+
+        }
+        binding.subjectsRecyclerview.setHasFixedSize(true)
+        binding.subjectsRecyclerview.layoutManager = GridLayoutManager(requireContext(), 2)
+        binding.subjectsRecyclerview.adapter = subjectAdapter
     }
 
     private fun setUpObserver() {
@@ -31,7 +42,12 @@ class DashBoardFragment : BaseFragment(R.layout.fragment_dashboard) {
                     Log.e("NETWORK", "Error ${it.message}")
                 }
                 is NetworkStatus.Success -> {
-                    Log.e("NETWORK", "Success data ${it.data}")
+                    subjectAdapter.addSubjects(it.data!!.subjects)
+                    binding.subjectShimmer.shimmerRoot.visibility = View.GONE
+                    val constraintSet = ConstraintSet()
+                    constraintSet.clone(binding.root)
+                    constraintSet.connect(R.id.recent_title, ConstraintSet.TOP, R.id.subjects_recyclerview, ConstraintSet.BOTTOM, 25)
+                    constraintSet.applyTo(binding.root)
                 }
             }
         })
